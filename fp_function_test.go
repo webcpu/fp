36da546890f401a5975b7b3aac090e10ec36f8e1
liang <liang@unchartedworks.com>
2021-07-29 20:46:51 +0200

    feature: Construct, Composition

--- fp_function_test.go
+++ fp_function_test.go
@@ -8,15 +8,36 @@ import (
 
 var _ = Describe("function", func() {
 	Context("Apply(f, expr)", func() {
-		It("Range", func() {
+		It("1 parameter", func() {
 			xs := Apply(Range, []interface{}{5})
 			expected := []int{1, 2, 3, 4, 5}
 			Expect(xs).To(Equal(expected))
 		})
 
-		It("reverse", func() {
-			xs := Apply(Reverse, []interface{}{Range(5)})
-			expected := Reverse([]int{1, 2, 3, 4, 5})
+		It("return 0 value", func() {
+			x := 1
+			f := func() { x += 1 }
+			Apply(f, []interface{}{})
+			Expect(x).To(Equal(2))
+		})
+
+		It("return 1 value", func() {
+			actual := Apply(Reverse, []interface{}{Range(5)})
+			expected := []interface{}{5,4,3,2,1}
+			Expect(actual).To(Equal(expected))
+		})
+
+		It("return 2 values", func() {
+			f := func() int {return 3}
+			xs := Apply(Timing, []interface{}{f})
+			expected := 3
+			Expect(Last(xs)).To(Equal(expected))
+		})
+
+		It("return 3 values", func() {
+			f := func() (int, int, int) {return 1,2,3}
+			xs := Apply(f, []interface{}{})
+			expected := []interface{}{1,2,3}
 			Expect(xs).To(Equal(expected))
 		})
 	})
@@ -26,6 +47,51 @@ var _ = Describe("function", func() {
 			xs := Construct(Range, 5)
 			expected := []int{1, 2, 3, 4, 5}
 			Expect(xs).To(Equal(expected))
+		})
+
+		It("sum", func() {
+			sum := func(nums ...int) (int, int, int) {
+				total := 0
+				for _, num := range nums {
+					total += num
+				}
+				n := len(nums)
+				mean := total/n
+				return n, mean, total
+			}
+			xs := Construct(sum, 1,2,3,4,5)
+			expected := []interface{}{5,3,15}
+			Expect(xs).To(Equal(expected))
+		})
+	})
+
+	Context("Composition(f, g, h...)", func() {
+		It("0 function", func() {
+			f := Composition()
+			actual := f(4)
+			expected := 4
+			Expect(actual).To(Equal(expected))
+		})
+
+		It("1 function", func() {
+			f := Composition(Range)
+			actual := f(4)
+			expected := []int{1,2,3,4}
+			Expect(actual).To(Equal(expected))
+		})
+
+		It("2 functions", func() {
+			f := Composition(Reverse, Range)
+			actual := f(4)
+			expected := []interface{}{4,3,2,1}
+			Expect(actual).To(Equal(expected))
+		})
+
+		It("3 functions", func() {
+			f := Composition(Reverse, Reverse, Range)
+			actual := f(4)
+			expected := []interface{}{1,2,3,4}
+			Expect(actual).To(Equal(expected))
 		})
 	})
 })
--- function.go
+++ function.go
@@ -3,15 +3,16 @@ package fp
 import (
 	"fmt"
 	"reflect"
+	"runtime"
 )
 
 func Apply(f interface{}, expr interface{}) interface{} {
 	sv := reflect.ValueOf(expr)
 	fv := reflect.ValueOf(f)
 	mustBe(fv, reflect.Func)
-	if !(sv.Kind() == reflect.Array || sv.Kind() == reflect.Slice) {
-		return expr
-	}
+	//if !(sv.Kind() == reflect.Array || sv.Kind() == reflect.Slice) {
+	//	return expr
+	//}
 	mustBeArraySlice(sv)
 
 	if !fv.Type().IsVariadic() {
@@ -29,7 +30,8 @@ func Apply(f interface{}, expr interface{}) interface{} {
 	for i := 0; i < len(values); i++ {
 		values[i] = reflect.ValueOf(sv.Index(i).Interface())
 	}
-	return fv.Call(values)[0].Interface()
+
+	return returnResult(fv, values, sv)
 }
 
 func Construct(f interface{}, args... interface{}) interface{} {
@@ -37,20 +39,81 @@ func Construct(f interface{}, args... interface{}) interface{} {
 	fv := reflect.ValueOf(f)
 	mustBe(fv, reflect.Func)
 
-	//if !fv.Type().IsVariadic() {
-	//	for i := 0; i < fv.Type().NumIn(); i++ {
-	//		left := fv.Type().In(i)
-	//		right := reflect.ValueOf(sv.Index(i).Interface()).Type()
-	//		if left.String() != "interface {}" && left != right {
-	//			msg := fmt.Sprintf("Apply: arguments[%v]'s type should be %v but not %v.", i, left, right)
-	//			panic(msg)
-	//		}
-	//	}
-	//}
+	if !fv.Type().IsVariadic() {
+		for i := 0; i < fv.Type().NumIn(); i++ {
+			left := fv.Type().In(i)
+			right := reflect.ValueOf(sv.Index(i).Interface()).Type()
+			if left.String() != "interface {}" && left != right {
+				msg := fmt.Sprintf("Construct: arguments[%v]'s type should be %v but not %v.", i, left, right)
+				panic(msg)
+			}
+		}
+	}
 
 	values := make([]reflect.Value, sv.Len(), sv.Len())//reflect.MakeSlice(elementType, sv.Len(), sv.Len())
 	for i := 0; i < len(values); i++ {
 		values[i] = reflect.ValueOf(sv.Index(i).Interface())
 	}
-	return fv.Call(values)[0].Interface()
+
+	return returnResult(fv, values, sv)
+}
+
+func returnResult(fv reflect.Value, values []reflect.Value, sv reflect.Value) interface{} {
+	result := fv.Call(values)
+	switch len(result) {
+	case 0:
+		return nil
+	case 1:
+		return result[0].Interface()
+	default:
+		output := reflect.MakeSlice(reflect.SliceOf(sv.Type().Elem()), len(result), len(result))
+		for i := 0; i < len(result); i++ {
+			output.Index(i).Set(result[i])
+		}
+		return output.Interface()
+	}
+}
+
+func Composition(fs... interface{}) func(...interface{}) interface{} {
+	if len(fs) == 0 {
+		return func (args... interface{}) interface{} {
+			if len(args) == 1 {
+				return Identity(args[0])
+			} else {
+				msg := "Identity: Identity called with #{len(args)} arguments; 1 argument is expected."
+				panic(msg)
+			}
+		}
+	}
+
+	for i := 0; i < len(fs); i++ {
+		f := fs[i]
+		fv := reflect.ValueOf(f)
+		if  fv.Kind() != reflect.Func {
+			msg := fmt.Sprintf("Composition: %v is not a function.", getFunctionName(f))
+			panic(msg)
+		}
+	}
+
+	fn := func(args... interface{}) interface{} {
+		sv := reflect.ValueOf(args)
+		var values []reflect.Value = make([]reflect.Value, len(args), len(args))
+		for i := 0; i < len(values); i++ {
+			values[i] = reflect.ValueOf(args[i])
+		}
+		for i := len(fs) - 1; i >= 0; i-- {
+			f := fs[i]
+			fv := reflect.ValueOf(f)
+			if i > 0 {
+				values = fv.Call(values)
+			}
+		}
+
+		return returnResult(reflect.ValueOf(fs[0]), values, sv)
+	}
+	return fn
+}
+
+func getFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
